d5de797812736abd983ccc9bc115c31ba7f64855
Resül Çay <resulcayop@gmail.com>
2023-12-17 03:22:16 +0300

    Logging and dbcontext donfigs are added

--- EFCoreTutorial.WebApi/Program.cs
+++ EFCoreTutorial.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using EFCoreTutorial.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddLogging();
+builder.Services.AddDbContext<ApplicationDbContext>(conf =>
+{
+	conf.UseSqlServer("");
+	conf.EnableSensitiveDataLogging();
+});
 
 var app = builder.Build();
 
